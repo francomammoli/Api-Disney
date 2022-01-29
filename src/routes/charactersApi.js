69a1157b670cf95a1757372ec7da14893894600b
francomammoli <francomammoli@hotmail.com>
2022-01-29 02:32:24 -0300

    rutas personajes

---
+++ src/routes/charactersApi.js
@@ -0,0 +1,38 @@
+const {Router} = require('express');
+const router = Router();
+//requerimos multer para la subida de imagenes
+const multer = require('multer');
+const path = require('path');
+
+//require controller
+const personajeController = require('../controllers/charactersApi');
+
+//config de multer
+const storage = multer.diskStorage({
+    destination:(req,file,cb) =>{
+        cb(null,'./src/public/imagenes');
+    },
+    filename : (req,file,cb) => {
+        let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
+        cb(null, fileName);
+    }
+});
+
+const uploadFile = multer({storage});
+
+//listar
+router.get('/',personajeController.show);
+
+//detalle
+router.get('/:id',personajeController.detail);
+
+//crear 
+router.post('/',uploadFile.single('imagenes'),personajeController.create);
+
+//edit
+router.post('/:id',uploadFile.single('imagenes'),personajeController.edit);
+
+//delete
+router.delete('/:id',personajeController.delete);
+
+module.exports = router;
